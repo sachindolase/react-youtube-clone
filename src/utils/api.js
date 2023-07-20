cc67244bd093d96a426b6a85c4fd77b18ffdfac8
sachindolase <sachindolase@gmail.com>
2023-07-20 14:56:39 +0530

    Youtube-clone-react18

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "ebdc45f6f3msha1490d60fabbb7fp1026a5jsn0641f6a9c872",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
